608cf3df963b4f00219763de1513a1cd4f0121f4
Feynman Liang <feynman.liang@gmail.com>
2015-11-10 18:00:00 +0000

    Adds quoted characters

--- CSVParser.hs
+++ CSVParser.hs
@@ -1,16 +1,45 @@
 {-# OPTIONS -Wall -fwarn-tabs #-}
 module CSVParser where
 
+import Data.Functor.Identity (Identity)
+import Text.Parsec.Prim (ParsecT)
 import Text.ParserCombinators.Parsec
 
+csvFile :: ParsecT String u Identity [[String]]
 csvFile = endBy line eol
-line = sepBy cell (char ',')
-cell = many (noneOf ",\n")
 
+line :: ParsecT String u Identity [String]
+line = sepBy cell (char ',')
+
+cell :: ParsecT String u Identity String
+cell = quotedCell <|> many (noneOf ",\n")
+
+quotedCell :: ParsecT String u Identity String
+quotedCell =
+  do _ <- char '"'
+     content <- many quotedChar
+     _ <- char '"' <?> "quote at end of cell"
+     return content
+
+quotedChar :: ParsecT String u Identity Char
+quotedChar =
+  noneOf "\""
+  <|> try (string "\"" >> return '"')
+
+eol :: ParsecT String u Identity String
 eol = try (string "\n\r")
   <|> try (string "\r\n")
   <|> string "\n"
   <|> string "\r"
+  <?> "end of line"
 
 parseCSV :: String -> Either ParseError [[String]]
-parseCSV input = parse csvFile "(unknown)" input
+parseCSV = parse csvFile "(unknown)"
+
+main :: IO ()
+main =
+  do c <- getContents
+     case parse csvFile "(stdin)" c of
+       Left e -> do putStrLn "Error parsing input:"
+                    print e
+       Right r -> mapM_ print r
